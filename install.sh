5135cf7bd25020d7bb8ee5ce26e06debb4c3090c
Alexander Ryzhov <github@ryzhov-al.ru>
2016-02-29 14:29:03 +0300

    install.sh: cosmetics

--- install.sh
+++ install.sh
@@ -12,9 +12,9 @@ dl () {
     echo -n "Downloading $2... "
     wget -q $1 -O $2
     if [ $? -eq 0 ] ; then
-	echo "success!"
+	echo 'success!'
     else
-	echo "failed!"
+	echo 'failed!'
     exit 1
     fi
     [ -z "$3" ] || chmod +x $2
@@ -23,7 +23,7 @@ dl () {
 cd /opt
 
 if [ ! -e '/opt/bin/opkg' ] ; then
-    # Entware is not installed, install and start scripts
+    # Entware is not installed, install start scripts
     mkdir -p /opt/etc/init.d
     dl http://pkg.entware.net/binaries/mipsel/installer/rc.func /opt/etc/init.d/rc.func
     dl http://pkg.entware.net/binaries/mipsel/installer/rc.unslung /opt/etc/init.d/rc.unslung x
@@ -36,13 +36,17 @@ fi
 dl https://raw.githubusercontent.com/ryzhovau/padavan-debian/master/opt/etc/init.d/S99debian /opt/etc/init.d/S99debian x
 dl http://files.ryzhov-al.ru/Routers/chroot-debian/debian_clean.tgz /opt/debian_clean.tgz
 
-echo 'Unpacking Debian environment...'
-tar -xzf debian_clean.tgz && rm debian_clean.tgz
+echo -n 'Unpacking Debian environment... '
+tar -xzf debian_clean.tgz
+if [ $? -eq 0 ] ; then
+    echo 'success!'
+else
+    echo 'failed!'
+    exit 1
+fi
+rm debian_clean.tgz
 echo 'ssh' > /opt/debian/chroot-services.list
 
-cat << EOF
-
-The Debian service from /opt/etc/chroot-services.list will be started automatically at boot time. You may log into Debian environment via SSH root:debian@192.168.0.1:65022. Do you wish to start it now? [y/n]
-EOF
+echo -n 'The Debian service from /opt/debian/chroot-services.list will be started automatically at boot time. You may log into Debian environment via SSH root:debian@192.168.0.1:65022. Do you wish to start it now? [y/n]: '
 read yn
 [ "$yn" = "y" ] && /opt/etc/init.d/S99debian start
